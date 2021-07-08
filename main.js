a06f139e4ac4fd319ab289edb17fa0ed3761a5ba
RAFA1211 <rafael.angarita.raar@gmail.com>
2021-07-08 09:31:26 -0500

    Repaso Variables Tipo de Datos02/07/21

---
+++ main.js
@@ -0,0 +1,12 @@
+nombreCompletoDelProfesor = `Rafael Angel Angarita`;
+miEdad = 47;
+altura = 170;
+soyUnEstudiante = true;
+miProfesion = `Ingeniero`
+
+
+console.log(nombreCompletoDelProfesor, typeof(`nombreCompletoDelProfesor`));
+console.log(miEdad, typeof(miEdad));
+console.log(altura, typeof(altura));
+console.log(soyUnEstudiante, typeof(soyUnEstudiante));
+console.log(miProfesion);
